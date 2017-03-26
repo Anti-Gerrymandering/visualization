35834386585ba9da34455fff63f7187091969209
Aaron <aaron.long.c@gmail.com>
2017-03-26 18:16:33 -0400

    Origin

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import '../sass/App.sass'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <div className='App-header'>
+          <h2>Visualizing Gerrymandering</h2>
+        </div>
+        <p className='App-intro'>
+          To get started, edit <code>src/components/App.js</code> and save to reload.
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App
