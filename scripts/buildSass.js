3f04cf39211911d6da3610747bb88d935a9d3851
Aaron <aaron.long.c@gmail.com>
2017-03-27 20:16:39 -0400

    Automated Sass build during build script

---
+++ scripts/buildSass.js
@@ -0,0 +1,31 @@
+const colors = require('colors').red
+const fs = require('fs')
+const spawn = require('child_process').spawnSync
+
+function sassBuilder () {
+  const cssOutPut = spawn('node-sass', ['App.sass'], { cwd: process.cwd() + '/src/sass' })
+  return cssOutPut.stdout.toString()
+}
+
+function mkdir (dir, callback) {
+  fs.stat(dir, (err, stats) => {
+    if (err && (err.errno === 34 || err.errno === -2)) {
+      console.log('Hey')
+      fs.mkdir(dir)
+    }
+    callback()
+  })
+}
+
+function main () {
+  const css = sassBuilder()
+  const buildDir = process.cwd() + '/build'
+  const build = () => {
+    fs.writeFile(process.cwd() + '/build/main.css', css, err => {
+      if (err) console.error(colors.bold('CSS wasn\'t produced'))
+    })
+  }
+  mkdir(buildDir, build)
+}
+
+main()
