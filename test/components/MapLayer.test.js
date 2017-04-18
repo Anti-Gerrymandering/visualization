a88d161287d6b07d2663c784d1fa124be1e12f6b
Anthony Ciacci <ciacci1234@gmail.com>
2017-04-17 23:14:49 -0400

    fixes crash tests
    Incorporates redux in app and maplayer
    components through the use of provider
    with store as a property. Crash tests
    now functional again.
    
    ISSUE: console.error
    TypeError:Network request failed at
    XMLHttpRequest.xhr.onerror...
    
    This issue occurs when tests run locally.

--- test/components/MapLayer.test.js
+++ test/components/MapLayer.test.js
@@ -2,8 +2,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import MapLayer from '../../src/components/MapLayer'
+import { Provider } from 'react-redux'
+import store from '../../src/configureStore'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<MapLayer />, div)
+  const map = <Provider store={store}><MapLayer /></Provider>
+  ReactDOM.render(map, div)
 })
