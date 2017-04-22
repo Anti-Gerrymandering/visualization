705a42e31f137444ca0731c1b848a0a6ba704915
Jonathan Mast <jon@padorpers.com>
2017-04-21 21:59:44 -0400

    Color districts according to compactness

--- src/components/GeoJsonUpdatable.js
+++ src/components/GeoJsonUpdatable.js
@@ -1,5 +1,27 @@
 import { GeoJSON } from 'react-leaflet'
 
+function getColorCompactness(c) {
+  return c > 0.54  ? '#ffffff' :
+    c > 0.45  ? '#ffffb2' :
+    c > 0.30  ? '#fed976' :
+    c > 0.15  ? '#feb24c' :
+    c > 0.10  ? '#fc4e2a' :
+    c > 0.05  ? '#e31a1c' :
+    '#b10026'
+}
+
+let updateDistrictColors = (leafletElement) => {
+  leafletElement.eachLayer((layer) => {
+    layer.setStyle({
+      fillColor: getColorCompactness(layer.feature.properties.Compactness),
+      weight: 1,
+      opacity: 1,
+      color: 'black',
+      fillOpacity: 0.8
+    })
+  })
+}
+
 /**
  * This module was coppied from:
  * https://github.com/open-austin/austingreenmap/blob/master/client/js/components/GeoJsonUpdatable.jsx
@@ -16,18 +38,9 @@ class GeoJsonUpdatable extends GeoJSON {
   componentDidUpdate (prevProps) {
     if (prevProps.data !== this.props.data) {
       // Adds new data to the map
-      this.leafletElement.addData(this.props.data)
     }
-
-    if (prevProps.visibleIds !== this.props.visibleIds) {
-      this.leafletElement.eachLayer((layer) => {
-        if (this.props.visibleIds.indexOf(layer.feature.id) === -1) {
-          layer.setStyle({fillOpacity: 0, opacity: 0})
-        } else {
-          layer.setStyle({fillOpacity: 0.4, opacity: 1})
-        }
-      })
-    }
+    this.leafletElement.addData(this.props.data)
+    updateDistrictColors(this.leafletElement)
   }
 }
 
